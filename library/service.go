f99fb58e53aade9d4111703101452e46a22813de
Gabriel Lee <gabrielleeyj@outlook.com>
2021-01-13 15:35:08 +0800

    preliminary structure.

--- library/service.go
+++ library/service.go
@@ -1,5 +1,7 @@
 package main
 
+import "errors"
+
 // Service interface is responsible for managing the various services that will be used.
 type Service interface {
 	ListAll() ([]Book, error)
@@ -14,4 +16,16 @@ type BookService struct {
 
 func ValidateISBN(isbn string) error {
 	return nil
+}
+
+func (s *BookService) ListAll() ([]Book, error) {
+	return nil, errors.New("No valid books")
+}
+
+func (s *BookService) Reserve(isbn string) (*Book, error) {
+	return nil, errors.New("Unable to reserve book.")
+}
+
+func (s *BookService) Return(isbn string) (*Book, error) {
+	return nil, errors.New("Unable to return book.")
 }
